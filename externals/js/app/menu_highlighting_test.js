2907923feafdcb57b17f4a9479745df7fbe7f2a7
Emily Myers <emilyhannahmyers@gmail.com>
2014-09-02 23:48:05 -0400

    updates logo file, moves testing to separate file

---
+++ externals/js/app/menu_highlighting_test.js
@@ -0,0 +1,13 @@
+    // on click, change active menu
+    // STILL IN TESTING OMG
+    // $(".launchpad div").click(function () {
+    //     var active_menu = $(this).attr("data-menuid");
+
+    //     console.log("the active_menu is " + active_menu);
+    //     active_menu.setAttribute("id", "active");
+
+    //     // highlights proper menu item
+    //     //var sidemenuLabel = side_menu.indexOf(sidemenu)
+    //     //console.log("the sidemenu label is " + sidemenuLabel);   
+    //     //sidemenuLabel.setAttribute("id", "active");
+    // });
--- externals/js/app/spotlight.js
+++ externals/js/app/spotlight.js
@@ -2,14 +2,10 @@
 var $ = window.$,
     jQuery = window.jQuery;
 
+// shows both the spotlight div and the clicked content
 function spotlightShow(content) {
     $(".spotlight").show();
     $("#" + content).show();
-}
-
-function spotlightHide() {
-    $(".spotlight").hide();
-    $(".spotlight").children().hide();
 }
 
 $(document).ready(function () {
@@ -18,41 +14,37 @@ $(document).ready(function () {
     // shows the menu on the side
     $(".side_menu").show();
 
-    // on click, change active menu
-    // STILL IN TESTING OMG
-    // $(".launchpad div").click(function () {
-    //     var active_menu = $(this).attr("data-menuid");
-
-    //     console.log("the active_menu is " + active_menu);
-    //     active_menu.setAttribute("id", "active");
-
-    //     // highlights proper menu item
-    //     //var sidemenuLabel = side_menu.indexOf(sidemenu)
-    //     //console.log("the sidemenu label is " + sidemenuLabel);   
-    //     //sidemenuLabel.setAttribute("id", "active");
-    // });
-
-    // when an image in the launchpad is clicked...
+    // expands an image from the launchpad
     $(".launchpad li").click(function () {
 
         // getting the alt tag
         var content = $(this).attr("data-content"),
             sidemenu = $("#" + content).attr("data-menu");
 
-        //console.log("the content is " + content + " from main function");
-        //console.log("the sidemenu connected to that is " + sidemenu);
+        console.log("the content is " + content + " from main function");
+        console.log("the sidemenu connected to that is " + sidemenu);
 
         // fades out the grid and in the image selected
         $(".launchpad").hide();
         spotlightShow(content);
+
+        // highlights the corresponding menu item
+        $(".menu").children()
+
     });
 
-    // when a menu item is clicked...
+    // minimizes photo back to main launchpad
+    $(".spotlight").click(function () {
+        $(".spotlight").hide();
+        $(".launchpad").show();
+    });
+
+    // changes what set of images is shown in the launchpad by menu item
     $(".side_menu").click(function () {
         // console.log("You clicked a menu item");
 
         // fades out content, and photo grid in
-        spotlightHide();
+        $(".spotlight").hide();
         $(".launchpad").show();
     });
 
