3a3171ff448bbefd03a25b754423b2ed420c9941
Emily Myers <emilyhannahmyers@gmail.com>
2014-09-03 22:19:02 -0400

    adds menu highlighting/unhighlighting on click

--- externals/js/app/menu_highlighting_test.js
+++ externals/js/app/menu_highlighting_test.js
@@ -1,13 +1,13 @@
-    // on click, change active menu
-    // STILL IN TESTING OMG
-    // $(".launchpad div").click(function () {
-    //     var active_menu = $(this).attr("data-menuid");
+//on click, change active menu
+//STILL IN TESTING OMG
+$(".launchpad div").click(function () {
+    var new_active_menu = $(this).attr("data-menuid");
 
-    //     console.log("the active_menu is " + active_menu);
-    //     active_menu.setAttribute("id", "active");
+    console.log("the new active_menu is " + new_active_menu);
+    new_active_menu.setAttribute("id", "active");
 
-    //     // highlights proper menu item
-    //     //var sidemenuLabel = side_menu.indexOf(sidemenu)
-    //     //console.log("the sidemenu label is " + sidemenuLabel);   
-    //     //sidemenuLabel.setAttribute("id", "active");
-    // });
+    //highlights proper menu item
+    var sidemenuLabel = $(".side_menu").indexOf(sidemenu);
+    console.log("the sidemenu label is " + sidemenuLabel);   
+    sidemenuLabel.setAttribute("id", "active");
+});
--- externals/js/app/spotlight.js
+++ externals/js/app/spotlight.js
@@ -14,6 +14,24 @@ function spotlightHide() {
     $(".spotlight").children().hide();
 }
 
+// highlights the correct menu item when an image is clicked
+function addMenuHighlight(menuToHighlight) {
+    var menuItem = document.getElementById(menuToHighlight);
+
+    menuItem.classList.add("active");
+
+    console.log(menuItem);
+    console.log("the menu to highlight is " + menuToHighlight);
+}
+
+// removes the highlighting on the menu when spotlight is clicked
+function removeMenuHighlight() {
+    // allDivsButPhotos.classList.toggle("active", false);
+
+    $("a.active").toggleClass();
+
+}
+
 $(document).ready(function () {
     "use strict";
 
@@ -25,25 +43,24 @@ $(document).ready(function () {
 
         // getting the alt tag
         var content = $(this).attr("data-content"),
-            sidemenu = $("#" + content).attr("data-menu"),
-            menuToHighlight = document.querySelector("data-menuid");
+            sidemenu = $("#" + content).attr("data-menu");
 
         console.log("the content is " + content + " from main function");
-        console.log("the sidemenu connected to that is " + sidemenu);
+        // console.log("the sidemenu connected to that is " + sidemenu);
 
         // fades out the grid and in the image selected
         $(".launchpad").hide();
         spotlightShow(content);
 
         // highlights the corresponding menu item
-        console.log("the correct menu to highlight is " + menuToHighlight);
-
+        addMenuHighlight(sidemenu);
     });
 
     // minimizes photo back to main launchpad
     $(".spotlight").click(function () {
         spotlightHide();
         $(".launchpad").show();
+        removeMenuHighlight();
     });
 
     // changes what set of images is shown in the launchpad by menu item
